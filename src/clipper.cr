508de5519b7d404b15f9a3b697272fb7e84046e7
Danny Ben Shitrit <db@dannyben.com>
2019-11-13 21:27:13 +0000

    add op watch

--- src/clipper.cr
+++ src/clipper.cr
@@ -1,6 +1,12 @@
 require "option_parser"
 
 class Clipper
+  getter :commands
+
+  def initialize
+    @commands = {} of String => Proc(Array(String), Nil)
+  end
+
   def flag(long, short = nil, default = false)
     key = long.split(' ').first
     input[key] = default
@@ -12,10 +18,26 @@ class Clipper
     arg_keys << name
   end
 
+  def command(name, &handler : Array(String) ->)
+    commands[name] = handler
+  end
+
   def parse(args = [] of String)
     args = args.dup
     parser.invalid_option { }
+
     parser.parse args
+
+    if args.size > 0 && commands.size > 0
+      input = args[0]
+      commands.each do |command, block|
+        if command == input
+          args.shift
+          block.call args
+        end
+      end
+    end
+
     append_named_args args
     input
   end
